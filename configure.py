0d6f3bf686e8dac517b61cc1a9576c3823da24d7
lcroy <lcroy@163.com>
2022-04-28 20:58:25 +0000

    Add existing file

--- configure.py
+++ configure.py
@@ -9,16 +9,26 @@ class Config:
         self.data_folder_path = os.path.join(self.project_path, 'data')
         self.raw_aursad_path = os.path.join(self.data_folder_path, 'aursad_tabular.dat')
 
-        # parameters
-        self.lr = 0.0001
-        self.loss = 'sparse_categorical_crossentropy'
-        self.epochs = 500
-        self.batch_size = 32
+        # parmeters == common
         self.num_class = 4
+        self.lr = 0.0001
+        self.loss = 'sparse_categorical_crossentropy'  
+        self.epochs = 200
+        self.batch_size = 32
+
+        # parameters == Transformer
+        self.head_size=256
+        self.num_heads=4
+        self.ff_dim=4
+        self.num_transformer_blocks=6
+        self.mlp_units=[128]
+        self.mlp_dropout=0.3
+        self.dropout=0.2
 
         # model path
         self.model_path = os.path.join(self.project_path, 'models')
         self.model_Conv1D_path = os.path.join(self.model_path, 'Conv1D', 'model.h5')
+        self.model_TRM_path = os.path.join(self.model_path, 'TRM', 'model.h5')
 
         # figures of loss and acc
         self.fig_path = os.path.join(self.project_path, 'loss_acc')
--- scrTRM.py
+++ scrTRM.py
@@ -1,5 +1,3 @@
-# Testing vision Transformer architecture...ongoing script...
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -34,6 +32,29 @@ def load_data(dataset):
     return X_train, X_test, Y_train, Y_test
 
 
+# plot the accuracy and loss
+def plot_loss_acc(history, cfg):
+    # plot accuracy figure
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig(cfg.acc_fig_path)
+    plt.show()
+
+    # plot loss figure
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig(cfg.loss_fig_path)
+    plt.show()
+
+
 def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
     # Normalization and Attention
     x = layers.LayerNormalization(epsilon=1e-6)(inputs)
@@ -50,7 +71,8 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
     x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
     return x + res
 
-def build_model(
+
+def TRM_scr(
     input_shape,
     head_size,
     num_heads,
@@ -75,91 +97,58 @@ def build_model(
 
 
 if __name__ == '__main__':
+
+    # get configure file
     cfg = Config()
+
     # split data
     x_train, x_test, y_train, y_test = load_data(cfg.raw_aursad_path)
 
+    # data sample shape
     input_shape = x_train.shape[1:]
 
-    model = build_model(
+    # initial a Transformer model
+    model = TRM_scr(
         input_shape,
-        head_size=256,
-        num_heads=4,
-        ff_dim=4,
-        num_transformer_blocks=4,
-        mlp_units=[128],
-        mlp_dropout=0.4,
-        dropout=0.25,
+        head_size=cfg.head_size,
+        num_heads=cfg.num_heads,
+        ff_dim=cfg.ff_dim,
+        num_transformer_blocks=cfg.num_transformer_blocks,
+        mlp_units=cfg.mlp_units,
+        mlp_dropout=cfg.mlp_dropout,
+        dropout=cfg.dropout,
         n_classes=cfg.num_class
     )
 
-    model.compile(
-        loss="sparse_categorical_crossentropy",
-        optimizer=keras.optimizers.Adam(learning_rate=1e-4),
-        metrics=["sparse_categorical_accuracy"],
-    )
+    # compile model
+    opt = tf.optimizers.Adam(cfg.lr)
+    model.compile(loss=cfg.loss, optimizer=opt, metrics='acc')
+
+    # get model summary
     model.summary()
 
+    # using early stopping for efficient
     callbacks = [keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)]
 
-    model.fit(
-        x_train,
-        y_train,
-        validation_split=0.2,
-        epochs=200,
-        batch_size=64,
-        callbacks=callbacks,
-    )
+    # training model 
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=cfg.epochs, batch_size=cfg.batch_size, callbacks=callbacks)
 
-    model.evaluate(x_test, y_test, verbose=1)
+    # save model
+    model.save(cfg.model_TRM_path)
 
+    # plot the acc and loss
+    plot_loss_acc(history, cfg)
 
-# convert to embedding vector sequence
-def pre_seq(image, patch_size, weight):
-    patch = F.unfold(image, kernel_size=patch_size, stride=patch_size).transpose(-1,-2)
-    patch_embedding = patch * weight
-    return patch_embedding
+    # get f1, precision and recall scores
+    model = keras.models.load_model(cfg.model_TRM_path)
 
-if __name__ == "__main__":
-    cfg = Config()
+    y_pred1 = model.predict(x_test)
+    y_pred = np.argmax(y_pred1, axis=1)
 
-    # test pre_seq
-    bs, ic, image_h, image_w = 1, 1, 1, 469
-    patch_size = 1
-    model_dim = 512
-    patch_depth = patch_size * patch_size * ic
-    seq = torch.randn(bs, ic, image_h, image_w)
-    weight = torch.randn(patch_depth, model_dim)
-    max_num_token = 470
-    num_classes = 4
-
-    label = torch.randint(4, (bs,))
-
-    # embedding sequence
-    patch_embedding = pre_seq(seq, patch_size, weight)
-
-    # add CLS token embedding
-    cls_token_embedding = torch.randn(bs, 1, model_dim, requires_grad=True)
-    token_embedding = torch.cat([cls_token_embedding, patch_embedding], dim=1)
-
-    # add position embedding
-    position_embedding_table = torch.randn(max_num_token, model_dim, requires_grad=True)
-    seq_len = token_embedding.shape[1]
-    position_embedding = torch.tile(position_embedding_table[:seq_len], [token_embedding.shape[0], 1, 1])
-    token_embedding += position_embedding
-
-    # pass embedding to transformer encoder
-    encoder_layer = nn.TransformerEncoderLayer(d_model=model_dim,nhead=8)
-    transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
-    encoder_output = transformer_encoder(token_embedding)
-
-    # do classification
-    cls_token_output = encoder_output[:, 0, :]
-    linear_layer = nn.Linear(model_dim, num_classes)
-    logits = linear_layer(cls_token_output)
-
-    loss_fn = nn.CrossEntropyLoss()
-    loss = loss_fn(logits, label)
-
-    print(loss)
-
+    # save f1, precision, and recall scores
+    scores = {"TRM_precision": precision_score(Y_test, y_pred, average="macro"),
+              "TRM_recall": recall_score(Y_test, y_pred, average="macro"),
+              "TRM_f1": f1_score(Y_test, y_pred, average="macro")}
+    with open(cfg.scores_file_path, 'a') as outfile:
+        json.dump(scores, outfile)
+    outfile.close()
